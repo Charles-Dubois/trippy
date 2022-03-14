dede70ff1b2768380d94b305d352a288b399676c
Charles Dubois <charles.dubois.h@gmail.com>
2022-03-14 12:35:06 +0100

    precedent work changed in middleware with guard

--- back/router/hotelsRouter.js
+++ back/router/hotelsRouter.js
@@ -1,15 +1,31 @@
 const data = require("../hotels.json");
 const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
+let hotelById = "";
+function handleHotelById(req, res, next) {
+  checkId = data.find((hotel) => {
+    return hotel.id.toString() === req.params.id.toString();
+  });
+
+  if (!checkId) {
+    return res
+      .status(400)
+      .json({
+        error: "error400 bad request",
+        description: `${req.params.id} id does not exists`,
+      });
+  }
+  hotelById = checkId;
+  next();
+}
+
 router.get("/", (_req, res) => {
   res.json(data);
 });
 
-router.get("/:id", (req, res) => {
-  const hotelById = data.find((hotel) => {
-    return hotel.id.toString() === req.params.id.toString();
-  });
-
+router.get("/:id", handleHotelById, (req, res) => {
   res.json(hotelById);
 });
+// router.patch("/:id", (req, res) => {});
 module.exports = router;
--- back/router/restaurantsRouter.js
+++ back/router/restaurantsRouter.js
@@ -1,14 +1,27 @@
 const data = require("../restaurants.json");
 const express = require("express");
 const router = express.Router();
-router.get("/", (_req, res) => {
-  res.json(data);
-});
-router.get("/:id", (req, res) => {
-  const restaurantById = data.find((restaurant) => {
+
+let restaurantById = "";
+function handleRestaurantById(req, res, next) {
+  checkId = data.find((restaurant) => {
     return restaurant.id.toString() === req.params.id.toString();
   });
 
+  if (!checkId) {
+    return res.status(400).json({
+      error: "error 400 bad request",
+      description: `${req.params.id} id does not exists`,
+    });
+  }
+  restaurantById = checkId;
+  next();
+}
+
+router.get("/", (_req, res) => {
+  res.json(data);
+});
+router.get("/:id", handleRestaurantById, (req, res) => {
   res.json(restaurantById);
 });
 module.exports = router;
