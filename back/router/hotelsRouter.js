d525ae3dfa5d44f4a891bd303e8d2d46e6e87a9a
Charles Dubois <charles.dubois.h@gmail.com>
2022-03-14 12:16:11 +0100

    path with id added to the routers

--- back/router/hotelsRouter.js
+++ back/router/hotelsRouter.js
@@ -4,4 +4,12 @@ const router = express.Router();
 router.get("/", (_req, res) => {
   res.json(data);
 });
+
+router.get("/:id", (req, res) => {
+  const hotelById = data.find((hotel) => {
+    return hotel.id.toString() === req.params.id.toString();
+  });
+
+  res.json(hotelById);
+});
 module.exports = router;
--- back/router/restaurantsRouter.js
+++ back/router/restaurantsRouter.js
@@ -4,5 +4,11 @@ const router = express.Router();
 router.get("/", (_req, res) => {
   res.json(data);
 });
+router.get("/:id", (req, res) => {
+  const restaurantById = data.find((restaurant) => {
+    return restaurant.id.toString() === req.params.id.toString();
+  });
 
+  res.json(restaurantById);
+});
 module.exports = router;
