3f562e810eaebe035de207502978670d274b588d
Charles-Dubois <charles.dubois.h@gmail.com>
2022-03-15 11:59:34 +0100

    generating user key api added

--- back/router/premiumRouter.js
+++ back/router/premiumRouter.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const postPremium = require("./JoiConditions/postPremium");
+const data = [];
 
-router.get("/", (req, res) => {
-  res.send("hello world");
-});
+function checkUsername(req, res, next) {
+  const validation = postPremium.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      message: "error 400 bad request",
+      description: validation.error.details[0].message,
+    });
+  }
+  next();
+}
 
-router.post("/", (req, res) => {
-  res.status(201).send("premium added");
+router.post("/", checkUsername, (req, res) => {
+  const userKey = { username: req.body.username, APIKey: uuidv4() };
+  data.push(userKey);
+  res.status(201).send({ message: "User added", details: userKey });
 });
 module.exports = router;
